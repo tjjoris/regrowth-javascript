1d6c8545b6ec4dde20833d17d0a07c9eff2f5cc8
tjjoris <tjjoris@gmail.com>
2024-06-28 22:57:15 -0400

    trying to pass extra healing other units but it's making hp NaN.

--- regrowth.js
+++ regrowth.js
@@ -30,7 +30,7 @@ function setGame () {
         tile[i].isShip = true;
         tile[i].buildMax = 3;
         tile[i].buildCurrent = 0;
-        tile[i].hpMax = 10000;
+        tile[i].hpMax = 25;
         tile[i].hpCurrent = tile[i].hpMax;
         //click a ship to turn it into a circle.
         tile[i].clicked = function clicked(){
@@ -40,18 +40,25 @@ function setGame () {
         }
         //update unit.
         tile[i].update = function update(){
+            //this will update either version of the unit
             this.updateByStatus();
+            //dispaly info for unit.
             indicator[i].innerHTML = tile[i].hpCurrent.toString();
-            console.log(this.hpCurrent)
         }
         tile[i].updateByStatus = function updateByStatus(){
+            //if ship, lower hp
             if (this.isShip == true){
                 this.lowerShipHP();
                 return;
             }
+            //change to ship
             this.updateCircle();
         }
         tile[i].lowerShipHP = function lowerShipHP(){
+            if (tile[i].hpCurrent <= 0) {
+                this.beginRegrowth();
+                return;
+            }
         //lower ship hp.
             this.hpCurrent -= 1;
             return;
@@ -61,10 +68,10 @@ function setGame () {
             if (this.buildCurrent >= this.buildMax) {
                 this.isShip = true;
                 this.ship.src = "./ship.png";
+                this.addInitialHPFromRegrowth();
                 return;
             }//increase build counter.
             this.buildCurrent ++;
-            console.log(this.buildCurrent);
             return;
         }
         //begin regrowth
@@ -73,6 +80,54 @@ function setGame () {
             this.ship.src = "./circle.png";
             this.buildCurrent = 0;
 
+        }
+        //add initial hp from regrowth
+        tile[i].addInitialHPFromRegrowth = function addInitailHPFromRegrowth(){
+            //amount to add to current unit.
+            let hpToAddToCurrent = this.hpMax;
+            //check if hp is over max
+            if (this.hpCurrent + this.hpMax > this.hpMax){
+                //amount to add to current unit
+                hpToAddToCurrent = this.hpMax - this.hpCurrent;
+                //amount to add to other units.
+                let extraHP = this.hpMax - this.hpToAddToCurrent;
+                //the amount to heal the 1 of the 2 units in the first round.
+                let halfExtra = extraHP / 2;
+                //heal the first round.
+                extraHP = tile[i].healOtherUnits(halfExtra, i);
+                //heal the second round.
+                tile[i].healOtherUnits(extraHP, i);
+            }            
+        }
+        //heal all other units.
+        tile[i].healOtherUnits = function healOtherUnits(amountToHeal, indexNotToHeal){
+            //if amount to heal is 0 do nothing
+            if (amountToHeal <= 0) {
+                return;
+            }
+            //the amount left over after healing other units.
+            let extraAfterHealingOhters =0;
+            //loop all units other than the passed unit.
+            for (let j=0; j<3; j++){
+                if (indexNotToHeal == j) { 
+                    continue;
+                }
+                extraAfterHealingOhters += tile[j].healFromOtherUnit(amountToHeal);
+            }
+            return extraAfterHealingOhters;
+        }
+        //extra healing for a single unit.
+        tile[i].healFromOtherUnit = function healFromOtherUnit(amountToHeal){
+           //check amoutn to heal is greater than 0.
+            if (amountToHeal <= 0) {
+                return;
+            }
+            this.hpCurrent += amountToHeal;
+            let extra = 0;
+            if (this.hpCurrent > this.hpMax){
+                extra = this.hpCurrent - this.hpMax;
+            }
+            return extra;
         }
         //event listener for clicking tile.
         tile[i].addEventListener("click", tile[i].clicked);
